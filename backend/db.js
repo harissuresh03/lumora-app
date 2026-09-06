4d96d8dbcbce9e0869c89f0982475dcf31ccebd4
harissuresh03 <harissuresh03@gmail.com>
2026-09-06 22:51:20 +0800

    minor db.js changes

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,18 @@
+const fs = require("fs");
+const path = require("path");
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "", // leave empty
-  database: "mental_health_app",
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "",
+  database: process.env.DB_NAME || "mental_health_app",
+  port: process.env.DB_PORT || 3306,
+
+  ssl: {
+    rejectUnauthorized: true,
+    ca: fs.readFileSync(path.join(__dirname, "ca.pem")),
+  },
 });
 
 db.connect((err) => {
